4be24d6e29a1b2c1b4a765dcaf468705d3e03a25
Anmol Desai <i-anmol@edmodo.com>
2017-02-03 20:07:50 -0800

    Added model test validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@user = User.new(name: "Anmol Desai", email: "anmoldesai@gmail.com")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
 end
